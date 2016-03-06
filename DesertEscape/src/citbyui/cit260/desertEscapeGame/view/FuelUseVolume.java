d66a294e2f83c77078addbf5e9cf13d84f1fa498
USER <USER@PCLAPTOPS>
2016-03-06 18:25:36 -0500

    March 8, modifications to correct use of menues and control of the super classes and MainMenuView and other be good

--- DesertEscape/src/citbyui/cit260/desertEscapeGame/view/FuelUseVolume.java
+++ DesertEscape/src/citbyui/cit260/desertEscapeGame/view/FuelUseVolume.java
@@ -1,4 +1,4 @@
-3/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Paez &
+ * @author Paez
  */
 public class FuelUseVolume {
 
